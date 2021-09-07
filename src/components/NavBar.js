a6574395c41af4ffc212c538e17f6926f06962a6
matiasjgelpi <matiasjgelpi@gmail.com>
2021-09-07 11:17:29 -0300

    cambios menores

---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+const NavBar = () => {
+return (<nav>
+            <img src="" alt="Logo-nina"/>
+            <ul class="">
+            <li class="">
+                <a class="" href="../../public/index.html">Quines soy</a>
+            </li>
+            <li class="">
+                <a class="" href="../../public/index.html">Fotos</a>
+            </li>
+            <li class="">
+                <a class="" href="../../public/index.html">Carrito</a>
+             </li>
+            <li class="">
+                <a class="" href="../../public/index.html">Contacto</a>
+            </li>
+         </ul>
+
+        </nav>);
+}
+
+export default NavBar;
